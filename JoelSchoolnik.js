d668a90700fe739f455606178b20730e9dbfd7f1
tivrama <tivrama@yahoo.com>
2016-12-12 08:55:55 -0800

    CSS and code cleanup

--- JoelSchoolnik.js
+++ JoelSchoolnik.js
@@ -9,51 +9,126 @@ var USER_INPUT_ARRAY_OF_EVENTS = [{start: 30, end: 150}, {start: 540, end: 600},
 
 
 
+
+// Object which tracks schedule with events.  
+var scheduleState = {
+  schedule: []
+};
+
 // --- Parent function ----------------------------------------
 
-var layOutDay = function(schedule) {
-  // Make sure input is an array and has at least item in it.  
-  if (!Array.isArray(schedule) || !schedule.length) {
-    console.error('incorrect input - not an array or has no events: ', schedule);
+var layOutDay = function(userInput) {
+  // Make sure input is an array and has at least item in it, and has a sart and end param.  
+  if (!Array.isArray(userInput) || !userInput.length) {
+    console.error('incorrect input - not an array or has no events: ', userInput);
     return 'error';
   }
-  if (!schedule[0].start || !schedule[0].end) {
-    console.error('incorrect input - events hav no start or end: ', schedule);
+  if (!userInput[0].start || !userInput[0].end) {
+    console.error('incorrect input - events hav no start or end: ', userInput);
     return 'error';
   }
-  // sort the input
-  scheduleState.schedule = schedule.sort(function(a, b){return a.start - b.start});
-
+  // add new events and sort
+  // -------------------------------------
+  scheduleState.schedule = []; // Comment this line out if we want to add events to an already existant schedule.  You can use clearSchedule() to reset.  
+  // -------------------------------------
+  scheduleState.schedule = scheduleState.schedule.concat(userInput)
+  scheduleState.schedule = scheduleState.schedule.sort(function(a, b){return a.start - b.start});
+  
   // call setW to add widths to events
   setW();
-
+console.log('state: ', scheduleState.schedule);
   // call populateDOM to append events
   populateDOM();
 
   return scheduleState;
 };
 
-// Object which tracks schedule with events, and can append attributes to append to DOM.  
-var scheduleState = {
-  schedule: []
-};
 
 var setW = function() {
-  var clock = 0;
-  for (let i = 0; i < scheduleState.schedule.length; i++) {
-    // create W param on each event
+  var schLength = scheduleState.schedule.length;
+  for (let i = 0; i < schLength; i++) {
+    // create or reset W param on each event
+    // TODO: Refactor to a recursive function. 
     scheduleState.schedule[i].W = 1;
+    scheduleState.schedule[i].P = 1; // P is for the horizontal position of the card
     if (scheduleState.schedule[i - 1]) {
       if (scheduleState.schedule[i].start < scheduleState.schedule[i - 1].end) {
         scheduleState.schedule[i].W = 2;
         scheduleState.schedule[i - 1].W = 2;
+        scheduleState.schedule[i].P = 1;
+        scheduleState.schedule[i - 1].P = 0;
+      }
+      if (scheduleState.schedule[i - 2]) {
+        if (scheduleState.schedule[i].start < scheduleState.schedule[i - 2].end) {
+          scheduleState.schedule[i].W = 3;
+          scheduleState.schedule[i - 1].W = 3;
+          scheduleState.schedule[i - 2].W = 3;
+          scheduleState.schedule[i].P = 2;
+          scheduleState.schedule[i - 1].P = 1;
+          scheduleState.schedule[i - 2].P = 0;
+        }
+        if (scheduleState.schedule[i - 3]) {
+          if (scheduleState.schedule[i].start < scheduleState.schedule[i - 3].end) {
+            scheduleState.schedule[i].W = 4;
+            scheduleState.schedule[i - 1].W = 4;
+            scheduleState.schedule[i - 2].W = 4;
+            scheduleState.schedule[i - 3].W = 4;
+            scheduleState.schedule[i].P = 3;
+            scheduleState.schedule[i - 1].P = 2;
+            scheduleState.schedule[i - 2].P = 1;
+            scheduleState.schedule[i - 3].P = 0;
+          }
+          if (scheduleState.schedule[i - 4]) {
+            if (scheduleState.schedule[i].start < scheduleState.schedule[i - 4].end) {
+              scheduleState.schedule[i].W = 5;
+              scheduleState.schedule[i - 1].W = 5;
+              scheduleState.schedule[i - 2].W = 5;
+              scheduleState.schedule[i - 3].W = 5;
+              scheduleState.schedule[i - 4].W = 5;
+              scheduleState.schedule[i].P = 4;
+              scheduleState.schedule[i - 1].P = 3;
+              scheduleState.schedule[i - 2].P = 2;
+              scheduleState.schedule[i - 3].P = 1;
+              scheduleState.schedule[i - 4].P = 0;
+            }
+            if (scheduleState.schedule[i - 5]) {
+              if (scheduleState.schedule[i].start < scheduleState.schedule[i - 5].end) {
+                scheduleState.schedule[i].W = 6;
+                scheduleState.schedule[i - 1].W = 6;
+                scheduleState.schedule[i - 2].W = 6;
+                scheduleState.schedule[i - 3].W = 6;
+                scheduleState.schedule[i - 4].W = 6;
+                scheduleState.schedule[i - 5].W = 6;
+                scheduleState.schedule[i].P = 5;
+                scheduleState.schedule[i - 1].P = 4;
+                scheduleState.schedule[i - 2].P = 3;
+                scheduleState.schedule[i - 3].P = 2;
+                scheduleState.schedule[i - 4].P = 1;
+                scheduleState.schedule[i - 5].P = 0;
+              }
+            }
+          }
+        }
       }
     }
   }
 };
 
+
+var removeElementsByClass = function(className){
+  var elements = document.getElementsByClassName(className);
+  while(elements.length > 0){
+    elements[0].parentNode.removeChild(elements[0]);
+  }
+};
+
+
 var populateDOM = function() {
-  var runningPositionH = 0;
+  // Remove all event cards from the board
+  removeElementsByClass("event");
+
+  var runningPositionV = 0;
+  var runningPositionH = 1;
   // Loop through scheduleState.schedule and create and apply elements
   for (let i = 0; i < scheduleState.schedule.length; i++) {
 
@@ -62,7 +137,7 @@ var populateDOM = function() {
     node.className = "event";
     var id = i;
     node.setAttribute("id", id);
-    var sampleItem = document.createTextNode("Sample Item, start: " + scheduleState.schedule[i].start + ", end: " + scheduleState.schedule[i].end); // or event name saved to event object
+    var sampleItem = document.createTextNode("Sample Item"); // or event name saved to event object
     node.appendChild(sampleItem);
 
     // Add Location to event card
@@ -72,37 +147,66 @@ var populateDOM = function() {
     location.setAttribute("id", id);
     var sampleLocation = document.createTextNode("Sample Location"); // or location saved to event object
     location.appendChild(sampleLocation);
-    // Figure out dimentions and position
-    var width = ((600 / scheduleState.schedule[i].W) - 20);
-    var height = scheduleState.schedule[i].end - scheduleState.schedule[i].start - 22;
-    if (!runningPositionH) {
-      runningPositionH = scheduleState.schedule[i].start - 30
+
+    // Set dimentions and vertical coordinates
+    var width = ((600 / scheduleState.schedule[i].W) - 25);
+    var height = scheduleState.schedule[i].end - scheduleState.schedule[i].start - 20;
+    if (!runningPositionV) {
       var top = scheduleState.schedule[i].start;
     } else {
-      var top = scheduleState.schedule[i].start - runningPositionH;
+      var top = scheduleState.schedule[i].start - runningPositionV;
     }
-    runningPositionH += scheduleState.schedule[i].end - scheduleState.schedule[i].start;
+    runningPositionV += scheduleState.schedule[i].end - scheduleState.schedule[i].start;
+
+    // Set horizontal coordinates
     var left = 0;
-    if (i === 2) {
-      left = width + 27;
+    // Set position based on W and previous divs
+    if (scheduleState.schedule[i].W > 1) {
+      if (scheduleState.schedule[i].P) {
+        left = (scheduleState.schedule[i].P * width) + 25
+      }
     }
+
+    //..works for example with W=2..
+    /*
+    if (scheduleState.schedule[i - 1]) {
+      if (scheduleState.schedule[i].W > 1 && scheduleState.schedule[i - 1].W > 1) {
+        if (scheduleState.schedule[i].start < scheduleState.schedule[i-1].end) {
+          left = width + 25;
+        }
+        if (runningPositionH > 1) {
+          left = 0
+        }
+        runningPositionH += 1;
+      }
+    }
+    */
 
     // Append to DOM
     document.getElementById("entry-board").appendChild(node);
-    document.getElementById(id).appendChild(location);
-    // Set dimentions and position on event
+    if (height > 25) { // Dont show location if card is too short
+      document.getElementById(id).appendChild(location);
+    }
+    // Apply dimentions and position on event
     document.getElementById(id).style.width = width+'px';
     document.getElementById(id).style.height = height+'px';
     document.getElementById(id).style.position = 'relative';
     document.getElementById(id).style.left = left+'px';
     document.getElementById(id).style.top = top+'px';
-
   }
-
 };
+
+// To clear schedule if using opting to keep adding events (see line 32)
+var clearSchedule = function() {
+  removeElementsByClass("event");
+  return scheduleState.schedule = [];
+};
+
 
 
 
 layOutDay(USER_INPUT_ARRAY_OF_EVENTS);
 
+
+// -- for tests ----------------
 // module.exports = { layOutDay };
--- README.md
+++ README.md
@@ -2,11 +2,8 @@
 - open terminal and cd into SAP-Coding-Challenge-Joel-Schoolnik
 - Open JoelSchoolnik.html in browser
 
-To populate schedule, enter the following in the console:
+To populate schedule:
 - open browser console
 - Paste in ```layOutDay(USER_INPUT_ARRAY_OF_EVENTS)```
+- Schedule can be cleared by running ```clearSchedule()```
 
-
-##Test:
-- open terminal and cd into SAP-Coding-Challenge-Joel-Schoolnik
-- Enter ```mocha``` in the terminal
