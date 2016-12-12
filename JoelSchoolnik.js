f893ac927b765026b1af8d58ad8207279f9a8e4b
tivrama <tivrama@yahoo.com>
2016-12-11 16:53:24 -0800

    basic DOM done.  Now build logic for positioning event cards

--- JoelSchoolnik.js
+++ JoelSchoolnik.js
@@ -1,8 +1,37 @@
-// To populate schedule, enter the following in the terminal:
-//   node
-//   var layOutDay = require('./JoelSchoolnik.js').layOutDay
-//   layOutDay(USER_INPUT_ARRAY_OF_EVENTS)
+// To populate schedule, paste the following in the console:
+// layOutDay(USER_INPUT_ARRAY_OF_EVENTS)
 
+// test example:
+var USER_INPUT_ARRAY_OF_EVENTS = [{start: 30, end: 150}, {start: 540, end: 600}, {start: 560, end: 620}, {start: 610, end: 670}];
+
+
+
+
+
+
+// --- Parent function ----------------------------------------
+
+var layOutDay = function(schedule) {
+  // Make sure input is an array and has at least item in it.  
+  if (!Array.isArray(schedule) || !schedule.length) {
+    console.error('incorrect input - not an array or has no events: ', schedule);
+    return 'error';
+  }
+  if (!schedule[0].start || !schedule[0].end) {
+    console.error('incorrect input - events hav no start or end: ', schedule);
+    return 'error';
+  }
+  // sort the input
+  scheduleState.schedule = schedule.sort(function(a, b){return a.start - b.start});
+
+  // call setW to add widths to events
+  setW();
+
+  // call populateDOM to append events
+  populateDOM();
+
+  return scheduleState;
+};
 
 // Object which tracks schedule with events, and can append attributes to append to DOM.  
 var scheduleState = {
@@ -27,32 +56,45 @@ var populateDOM = function() {
   var runningPositionH = 0;
   // Loop through scheduleState.schedule and create and apply elements
   for (let i = 0; i < scheduleState.schedule.length; i++) {
+
     // Create event element
     var node = document.createElement("div");
     node.className = "event";
     var id = i;
     node.setAttribute("id", id);
-    var sampleItem = document.createTextNode("Sample Item, start: " + scheduleState.schedule[i].start + ", end: " + scheduleState.schedule[i].end);
+    var sampleItem = document.createTextNode("Sample Item, start: " + scheduleState.schedule[i].start + ", end: " + scheduleState.schedule[i].end); // or event name saved to event object
     node.appendChild(sampleItem);
 
+    // Add Location to event card
+    var location = document.createElement("div");
+    location.className = "location";
+    var idL = i +'L';
+    location.setAttribute("id", id);
+    var sampleLocation = document.createTextNode("Sample Location"); // or location saved to event object
+    location.appendChild(sampleLocation);
     // Figure out dimentions and position
-    var width = (600 / scheduleState.schedule[i].W) - 10;
-
+    var width = ((600 / scheduleState.schedule[i].W) - 20);
+    var height = scheduleState.schedule[i].end - scheduleState.schedule[i].start - 22;
     if (!runningPositionH) {
       runningPositionH = scheduleState.schedule[i].start - 30
       var top = scheduleState.schedule[i].start;
     } else {
       var top = scheduleState.schedule[i].start - runningPositionH;
     }
-    var height = scheduleState.schedule[i].end - scheduleState.schedule[i].start - 22;
     runningPositionH += scheduleState.schedule[i].end - scheduleState.schedule[i].start;
+    var left = 0;
+    if (i === 2) {
+      left = width + 27;
+    }
+
     // Append to DOM
     document.getElementById("entry-board").appendChild(node);
+    document.getElementById(id).appendChild(location);
     // Set dimentions and position on event
     document.getElementById(id).style.width = width+'px';
     document.getElementById(id).style.height = height+'px';
     document.getElementById(id).style.position = 'relative';
-    // document.getElementById(id).style.left = left+'px';
+    document.getElementById(id).style.left = left+'px';
     document.getElementById(id).style.top = top+'px';
 
   }
@@ -61,41 +103,6 @@ var populateDOM = function() {
 
 
 
+layOutDay(USER_INPUT_ARRAY_OF_EVENTS);
 
-var layOutDay = function(schedule) {
-  // Make sure input is an array and has at least item in it.  
-  if (!Array.isArray(schedule) || !schedule.length) {
-    console.error('incorrect input - not an array or has no events: ', schedule);
-    return 'error';
-  }
-  if (!schedule[0].start || !schedule[0].end) {
-    console.error('incorrect input - events hav no start or end: ', schedule);
-    return 'error';
-  }
-
-  // sort the input
-  scheduleState.schedule = schedule.sort(function(a, b){return a.start - b.start});
-
-  // call setW to add widths to events
-  setW();
-
-  // call populateDOM to append events
-  populateDOM();
-
-  return 'Page Loaded';
-
-};
-
-
-
-
-var testSchedule2 = [
-  {start: 560, end: 620},
-  {start: 610, end: 670},
-  {start: 540, end: 600},
-  {start: 30, end: 150}
-];
-
-layOutDay(testSchedule2);
-
-module.exports = { layOutDay };
+// module.exports = { layOutDay };
--- README.md
+++ README.md
@@ -1,17 +1,12 @@
 ##Usage
-- In order to run Javascript files from command line, please install node.
 - open terminal and cd into SAP-Coding-Challenge-Joel-Schoolnik
 - Open JoelSchoolnik.html in browser
-To populate schedule, enter the following in the terminal:
--```node```
--```var layOutDay = require('./JoelSchoolnik.js').layOutDay```
--```layOutDay(USER_INPUT_ARRAY_OF_EVENTS)```
+
+To populate schedule, enter the following in the console:
+- open browser console
+- Paste in ```layOutDay(USER_INPUT_ARRAY_OF_EVENTS)```
 
 
-##Test: 
-Enter ```mocha``` in the terminal
-
-
--```node ```
--```var layOutDay  = require('/Users/tivrama/Desktop/SAP-Coding-Challenge-Joel-Schoolnik/JoelSchoolnik.js').layOutDay```
--```layOutDay(USER_INPUT_ARRAY_OF_EVENTS)```
+##Test:
+- open terminal and cd into SAP-Coding-Challenge-Joel-Schoolnik
+- Enter ```mocha``` in the terminal
